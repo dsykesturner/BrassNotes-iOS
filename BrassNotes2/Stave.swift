18a8cc57cef56522f72af31b5a42333601bc8143
Daniel Sykes-Turner <dst@localz.co>
2018-01-28 20:42:44 +1100

    Added flat/sharp symbols to notes

--- BrassNotes2/Stave.swift
+++ BrassNotes2/Stave.swift
@@ -18,6 +18,8 @@ class Stave: UIView {
     
     var note1: UIImageView! = UIImageView()
     var note2: UIImageView! = UIImageView()
+    var flat: UILabel! = UILabel()
+    var sharp: UILabel! = UILabel()
     var staveLines: [UIView]! = [UIView]()
     var aboveStaveLines: [UIView]?
     var belowStaveLines: [UIView]?
@@ -105,11 +107,23 @@ class Stave: UIView {
         // Update the note image/position
         note1.image = UIImage(named: "note")
         note2.image = UIImage(named: "note")
+        flat.text = "♭"
+        flat.textColor = UIColor.white
+        flat.font = UIFont(name: "Helvetica", size: 42)
+        sharp.text = "♯"
+        sharp.textColor = UIColor.white
+        sharp.font = UIFont(name: "Helvetica", size: 42)
         if note1.superview == nil {
             addSubview(note1)
         }
         if note2.superview == nil {
             addSubview(note2)
+        }
+        if flat.superview == nil {
+            addSubview(flat)
+        }
+        if sharp.superview == nil {
+            addSubview(sharp)
         }
         updateNotePosition(updatedNote: currentNote!)
     }
@@ -119,14 +133,37 @@ class Stave: UIView {
         let lineHeight:CGFloat = 2.0
         let heightBetweenLines = frame.size.height/4
         
-        // Image size (ratio) is 346 x 181 px
-        note1.frame = CGRect.init(x: imgClef.frame.size.width+50, y: CGFloat(updatedNote.stavePosition)*(heightBetweenLines)+lineHeight/2, width: heightBetweenLines/181*346, height: heightBetweenLines)
+        // Note area =         (stave width - clef width) / (2 notes) * (first 1/3 or 1.1/3 of the space) + stave width
+        let note1X = (self.frame.size.width - imgClef.frame.size.width) / 2 * (1/3) + imgClef.frame.size.width
+        let note2X = (self.frame.size.width - imgClef.frame.size.width) / 2 * (1+1/3) + imgClef.frame.size.width
+        // Sharp/flat area =   (stave width - clef width) / (2 notes) * (1.0 of the space) + stave width
+        let sharpX = imgClef.frame.size.width
+        let flatX = (self.frame.size.width - imgClef.frame.size.width) / 2 + imgClef.frame.size.width
         
+        // Image size (ratio) is 346 x 181 px
+        note1.frame = CGRect.init(x: note1X, y: CGFloat(updatedNote.stavePosition)*(heightBetweenLines)+lineHeight/2, width: heightBetweenLines/181*346, height: heightBetweenLines)
+        
+        // Only a second note is required if the note is flat and sharp
         if updatedNote.isFlat && updatedNote.isSharp {
             note2.isHidden = false
-            note2.frame = CGRect.init(x: imgClef.frame.size.width+150, y: CGFloat(updatedNote.stavePosition-0.5)*(heightBetweenLines)+lineHeight/2, width: heightBetweenLines/181*346, height: heightBetweenLines)
+            note2.frame = CGRect.init(x: note2X, y: CGFloat(updatedNote.stavePosition-0.5)*(heightBetweenLines)+lineHeight/2, width: heightBetweenLines/181*346, height: heightBetweenLines)
         } else {
             note2.isHidden = true
+        }
+        
+        let sizeDifferenceToNote:CGFloat = 1.5
+        if updatedNote.isSharp {
+            sharp.isHidden = false
+            sharp.frame = CGRect.init(x: sharpX, y: note1.frame.origin.y, width: note1.frame.size.width/sizeDifferenceToNote, height: note1.frame.size.height)
+
+        } else {
+            sharp.isHidden = true
+        }
+        if updatedNote.isFlat {
+            flat.isHidden = false
+            flat.frame = CGRect.init(x: flatX, y: note2.frame.origin.y, width: note2.frame.size.width/sizeDifferenceToNote, height: note2.frame.size.height)
+        } else {
+            flat.isHidden = true
         }
     }
     
